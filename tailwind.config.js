802f056eda79094acb9b0e8dd293057ff702024c
aaron-might <aaronmighty7@gmail.com>
2021-08-28 03:43:07 -0400

    adding background and keypad color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      textColor:{
+        skin:{
+          'key-base':'var(--color-text-key-base)',
+          'key-accent-1':'var(--color-text-key-accent-1)',
+          'key-accent-2':'var(--color-text-key-accent-2)'
+        },
+      },
+      backgroundColor: {
+        skin: {
+          base:'var(--color-fill-base)',
+          screen:'var(--color-fill-screen)',
+          keypad:'var(--color-fill-keypad)',
+          'key-base': 'var(--color-shadow-key-base)',
+          'key-accent-1': 'var(--color-fill-key-accent-1)',
+          'key-accent-2': 'var(--color-fill-key-accent-2)',
+        },
+      },
+      boxShadow: {
+        'key-base': '0px 4px var(--color-shadow-key-base)',
+        'key-accent-1': '0px 4px var(--color-shadow-key-accent-1)',
+        'key-accent-2': '0px 4px var(--color-shadow-key-accent-2)',
+      },
+      fontFamily: {
+        Spartan: 'Spartan, sans-serif',
+      },
+    },
   },
   variants: {
     extend: {},
