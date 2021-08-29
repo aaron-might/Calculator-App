7e1842e5ab3ff8fb8363e12e58e8ce1ffca2df7a
aaron-might <aaronmighty7@gmail.com>
2021-08-28 21:11:51 -0400

    write addeventlistener on themetoggle

---
+++ index.js
@@ -0,0 +1,11 @@
+const themeToggle = document.getElementById('theme-toggle');
+const body= document.querySelector('body');
+var theme= 1
+themeToggle.addEventListener('click', ()=>{
+    thee++;
+    console.log('theme: ', theme);
+})
+
+
+console.log(themeToggle);
+console.log(body);
