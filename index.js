dfb4e2dea3cb14e9ecc6d290136a7f8e35459d67
aaron-might <aaronmighty7@gmail.com>
2021-08-29 00:48:31 -0400

    writing switch statement for the theme class

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const themeToggle = document.getElementById('theme-toggle');
-const body= document.querySelector('body');
-var theme= 1
+const body = document.querySelector('body');
+var theme = 1;
+
 themeToggle.addEventListener('click', ()=>{
-    thee++;
+    theme++;
+    switch(theme){
+        case 2:
+            body.classList.add('theme-2')
+        break;
+        case 3:
+            body.classList.remove('theme-2')
+            body.classList.add('theme-3')
+            break;
+        case 4:
+            theme = 1;
+            body.classList.remove('theme-3');
+            break;
+
+    }
     console.log('theme: ', theme);
+    console.log(body);
 })
 
 
-console.log(themeToggle);
-console.log(body);
+// console.log(themeToggle);
+// console.log(body);
